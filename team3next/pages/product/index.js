860b849f97f3b57081da5fc9f9957dd66ff81638
chloe <whatever11854@gmail.com>
2023-09-24 16:35:45 +0800

    0924

---
+++ team3next/pages/product/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "@/components/layout/default-layout/navbar-main";
+
+export default function index() {
+  return (
+    <>
+      <Navbar />
+      <div>index</div>
+    </>
+  );
+}
