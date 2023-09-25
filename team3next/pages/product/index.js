5bc16e5217bcc3b8ac6d6a963aaa1744b348b0fb
chloe <whatever11854@gmail.com>
2023-09-25 17:47:29 +0800

    0925 product list

--- team3next/pages/product/index.js
+++ team3next/pages/product/index.js
@@ -1,21 +1,14 @@
 import React from "react";
 import Navbar from "@/components/layout/default-layout/navbar-main";
 import styles from "./index.module.css";
+import Bread from "@/components/product/bread";
 
 export default function index() {
   return (
     <>
       <Navbar />
       <div className="container">
-        <div className="bread bg-warning">
-          <span className="icon-home"></span>
-          <span className="icon-home"></span>
-          <a href="#">嗑零食</a>
-          <span className="icon-home"></span>
-          <a href="#">type</a>
-          <span className="icon-home"></span>
-          <a href="#typeLists"></a>
-        </div>
+        <Bread />
         <div className="w-100 d-flex ">
           <div className={styles.leftBox}>
             <div className={styles.left}>
@@ -30,19 +23,19 @@ export default function index() {
                 飲品/沖泡類 <span className="icon-home"></span>
               </button>
               <div className="collapse" id="type1">
-                <button className={styles.typeListBtn + "btn"} type="button">
+                <button className={styles.typeListBtn + " btn"} type="button">
                   茶類
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn"} type="button">
                   咖啡/咖啡豆
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn"} type="button">
                   果汁
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   醋/水果醋
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   酒類
                 </button>
               </div>
@@ -57,16 +50,16 @@ export default function index() {
                 烘焙食品/甜點
               </button>
               <div className="collapse" id="type2">
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   蛋糕/派
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   手工餅乾
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   麵包/吐司
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   奶酪/布丁/果凍
                 </button>
               </div>
@@ -81,19 +74,19 @@ export default function index() {
                 休閒零食
               </button>
               <div className="collapse" id="type3">
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   零食
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   糖果/巧克力
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   果醬/抹醬
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   果醬/抹醬
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   堅果/穀物
                 </button>
               </div>
@@ -108,16 +101,16 @@ export default function index() {
                 烹料料理
               </button>
               <div className="collapse" id="type4">
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   熟食/冷藏、冷凍食品
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   米/麵條
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   調理包/料理包
                 </button>
-                <button className={styles.typeListBtn + "btn "} type="button">
+                <button className={styles.typeListBtn + " btn "} type="button">
                   調味料/醬料
                 </button>
               </div>
@@ -132,13 +125,40 @@ export default function index() {
                 其他
               </button>
               <div className="collapse" id="type5">
-                <button className={styles.typeListBtn + "btn  "} type="button">
+                <button className={styles.typeListBtn + " btn  "} type="button">
                   其他
                 </button>
               </div>
             </div>
           </div>
-          <main className="bg-info w-100">123</main>
+          <main className="bg-info w-100">
+            <div
+              className={
+                styles.sort + " d-flex justify-content-end align-items-center"
+              }
+            >
+              <select
+                class="form-select form-select-sm d-inline-block"
+                aria-label="Small select example"
+              >
+                <option selected>排序</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+              <form class="d-flex align-items-center" role="search">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="搜尋"
+                  aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </main>
         </div>
       </div>
     </>
