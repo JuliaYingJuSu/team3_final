16bca99441d63ba85f8d7f2ba7236264da5ecc53
Alex <wyltw812@gmail.com>
2023-09-25 17:33:46 +0800

    0925

--- team3next/pages/restaurant/form-classic.js
+++ team3next/pages/restaurant/form-classic.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
 
@@ -5,10 +6,26 @@ export default function ClassicForm() {
   const [input, SetInput] = useState({ account: "", password: "" });
   const handleChange = (e) => {
     SetInput({...input,[e.target.name]:e.target.value})
+    console.log(input)
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await axios.post('http://localhost:3002/try-post', input
+      );
+      // 处理服务器响应
+      console.log('Server Response:', response.data);
+    } catch (error) {
+      // 处理请求错误
+      console.error('Error:', error);
+    }
+  };
+  
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" name="account" value={input.account} onChange={
         handleChange
       }  />
