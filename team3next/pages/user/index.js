bdf86d29b3287eb800eef226a8aa7c5a36b65fb7
will8086 <will8086@gmail.com>
2023-09-25 11:16:16 +0800

    修正BUTTON樣式

--- team3next/pages/user/index.js
+++ team3next/pages/user/index.js
@@ -5,6 +5,22 @@ export default function UserIndex() {
   return (
     <>
       <MyNavbar></MyNavbar>
+      <body>
+        <main className="container">
+          <div className="d-flex justify-content-between align-items-center mt-3">
+            <div className="middle flex-column ">
+                <h4>會員暱稱</h4>
+                <div className="mt-2">
+                    <span>110人</span>
+                    <span>追蹤中</span>
+                </div>
+                <div className="btn-middle mt-5">發表文章</div>
+            </div>
+            <div className="middle">1</div>
+            <div className="middle">2</div>
+          </div>
+        </main>
+      </body>
     </>
   );
 }
