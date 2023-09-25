581113206076c7131a8d69dd03b71b87b5a11e0e
will8086 <will8086@gmail.com>
2023-09-25 14:02:33 +0800

    會員資訊樣式完成

--- team3next/pages/user/index.js
+++ team3next/pages/user/index.js
@@ -6,10 +6,10 @@ export default function UserIndex() {
   return (
     <>
       <MyNavbar></MyNavbar>
-      <main className="container">
+      <main className="container bottom-line" style={{ paddingBottom: 40 }}>
         <div
-          className="d-flex justify-content-between align-items-center mt-3"
-          style={{ paddingInline: 250 }}>
+          className="d-flex justify-content-around align-items-center mt-3"
+          style={{ paddingInline: 200 }}>
           <div className="middle flex-column">
             <h4>會員暱稱</h4>
             <div className="mt-2">
@@ -19,14 +19,18 @@ export default function UserIndex() {
             <div className="btn-middle mt-5">發表文章</div>
           </div>
           <div className="middle">
-            <div>
-              <img
-                className="rounded-circle img-thumbnail headshot-big"
-                src="/images/logo.png"
-                alt="大頭照"></img>
+              <div className="rounded-circle img-thumbnail headshot-big position-relativ">
+                <span className="d-block position-absolute z-3" style={{paddingInlineStart:130,paddingTop:140}}>
+                  <button className="icon-pan fs-4 img-thumbnail rounded-circle"></button>
+                </span>
+              </div>
             </div>
+          <div className="d-flex" style={{ width: 250 }}>
+            用美食記錄生活點滴… 堆疊起美好的記憶…🚶‍♀️
+            所有的不期而遇💕都是美好的開始 喜歡黑咖啡☕的純粹美好💕
+            用美食記錄生活點滴… 堆疊起美好的記憶…🚶‍♀️
+            所有的不期而遇💕都是美好的開始 喜歡黑咖啡☕的純粹美好💕
           </div>
-          <div className="middle">2</div>
         </div>
       </main>
     </>
