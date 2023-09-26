ced47a63bf4231b4bde16315646db2b624e63575
will8086 <will8086@gmail.com>
2023-09-26 13:11:35 +0800

    一些變更

--- team3next/pages/user/user-article.js
+++ team3next/pages/user/user-article.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import MyNavbar from '../../components/layout/default-layout/navbar-main'
+import UserNavbar from "@/components/user/user-navbar";
+import Head from "next/head";
+import UserInfo from "@/components/user/user-info";
 
 export default function UserMyfrom() {
   return (
     <>
         <MyNavbar/>
+        <UserInfo></UserInfo>
+        <UserNavbar></UserNavbar>
+        <Head>
+        <title>我的文章</title>
+    </Head>
     </>
   )
 }
