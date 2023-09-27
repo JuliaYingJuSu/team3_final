11cec6429885b65cee7e83a7cedcf618b54865eb
Alex <wyltw812@gmail.com>
2023-09-27 15:08:56 +0800

    0927 afternoon

--- team3next/components/restaurant/restaurant-profile.js
+++ team3next/components/restaurant/restaurant-profile.js
@@ -1,13 +1,31 @@
 import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Profile() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+
   return (
     <>
-      <form action="">
+      <h2 style={{ color: "#985637" }}>會員資料管理</h2>
+      <form onSubmit={handleSubmit((data)=>{console.log(data)})}>
         <div className="d-flex flex-column">
-          <label htmlFor="">電子信箱</label>
-          <input type="text" />
+          <label htmlFor="email">電子信箱</label>
+          <input
+            type="text"
+            {...register("email", { required: "input something" })}
+            id="email"
+            placeholder="test@gmail.com"
+          />
+          <span></span>
         </div>
+        <button className="btn btn-big" type="submit">確認修改</button>
       </form>
     </>
   );
