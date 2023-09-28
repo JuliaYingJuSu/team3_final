e0ee1ecb9e3da0ed568ba74c2ef03e7513731313
will8086 <will8086@gmail.com>
2023-09-28 09:49:35 +0800

    toggle樣式

--- team3next/pages/user/login.js
+++ team3next/pages/user/login.js
@@ -3,6 +3,8 @@
 import React from "react";
 import Styles from "@/components/user/user-index.module.scss";
 import Head from "next/head";
+import Link from "next/link";
+import Toggle from "@/components/user/toggle";
 
 export default function Login() {
   return (
@@ -28,9 +30,18 @@ export default function Login() {
             borderTopLeftRadius: 241,
           }}>
           <div className="position-relativ">
-            <div className="position-absolute" style={{ right: 600, top: 130 }}>
+            <div className="position-absolute" style={{ right: 750, top: 130 }}>
               <div className="d-flex">
-               
+                <div className="middle">
+                  <Link href={"/"}>
+                    <span className="icon-home me-1"></span>
+                  </Link>
+                  <span className="icon-arrow-s-right"></span>
+                  <span>
+                    <Link href="#" className="text-dark fw-bold ms-1">登入</Link>
+                  </span>
+                </div>
+                <Toggle></Toggle>
               </div>
             </div>
           </div>
