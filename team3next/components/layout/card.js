ddb253ffb1cf414f136cdb712071e616e4157585
JuliaYingJuS <yingjumr@gmail.com>
2023-09-28 16:09:48 +0800

    卡片修改

--- team3next/components/layout/card.js
+++ team3next/components/layout/card.js
@@ -10,28 +10,8 @@ import MyArticle from "../post/my-article";
 export default function Card() {
   return (
     <>
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Modal title
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"></button>
-            </div>
-            <div className="modal-body">...</div>
-          </div>
-        </div>
-      </div>
+    <PostModol/>
+      
       <div className="col mt-2">
         <div className="card h-100 overflow-hidden">
           <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">
