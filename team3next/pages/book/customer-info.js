991e53a2706232a873c64e5732f8d14df6ae3e89
Hui Lun <monica30599@gmail.com>
2023-09-29 00:20:16 +0800

    修改 book

--- team3next/pages/book/customer-info.js
+++ team3next/pages/book/customer-info.js
@@ -9,95 +9,163 @@ export default function Index() {
         <title>食食嗑嗑-訂位人資料</title>
       </Head>
       <Navbar></Navbar>
-      <div className="container">
-        <div className="container2 ms-auto">
-          <div className="d-flex">
-            <div className="rowmb-2">
-              <label htmlFor="name" className="form-label col-form-label">
-                訂位人姓名
+      <div className="container mt-4">
+        <div className="d-flex justify-content-around align-items-center">
+          <div className="container2">
+            <img
+              className="w-100"
+              src="../../images/book/r1-1cut.png"
+              alt="..."
+            />
+            <div className="h5 d-flex justify-content-center py-4 con1">
+              <div className="pe-2 align-self-center">
+                <span className="icon-map"></span>
+              </div>
+              <div className="ms-1">
+                Cin Cin Osteria
+                <br />
+                請請義大利餐廳
+              </div>
+            </div>
+            <div className="d-flex justify-content-center align-items-center ">
+              <div className="fs18 my-3">
+                <div className="d-flex">
+                  <span className="pe-2">
+                    <span
+                      className="icon-member"
+                      style={{ fontSize: "16px" }}
+                    ></span>
+                  </span>
+                  <div className="ms-4">3 大 1 小</div>
+                </div>
+                <div className="d-flex mt-2">
+                  <span className="pe-2">
+                    <span className="icon-calender"></span>
+                  </span>
+                  <div className="ms-4">2023年11月7日 週二</div>
+                </div>
+                <div className="d-flex mt-2">
+                  <span className="pe-2">
+                    <span className="icon-bell"></span>
+                  </span>
+                  <div className="ms-4">17:00</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container3">
+            <div className="mb-4 form-check">
+              <input
+                className="form-check-input me-3"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label fs16" for="flexCheckDefault">
+                訂位人聯絡方式與登入的會員資料相同
+              </label>
+            </div>
+            <div className="mb-2 d-flex">
+              <div className="rowmb-2">
+                <label htmlFor="name" className="form-label col-form-label">
+                  訂位人姓名
+                </label>
+                <div className="col-12">
+                  <input
+                    type="text"
+                    className="inputframe name form-control"
+                    id="name"
+                    placeholder=" 請輸入姓名"
+                    autoFocus
+                  />
+                </div>
+              </div>
+              <div className="ms-4 mt-3 fs18 d-flex align-self-end">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="flexRadioDefault"
+                    id="flexRadioDefault1"
+                    checked
+                  />
+                  <label className="form-check-label" for="flexRadioDefault1">
+                    小姐
+                  </label>
+                </div>
+                <div className="form-check ms-2">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="flexRadioDefault"
+                    id="flexRadioDefault2"
+                  />
+                  <label className="form-check-label" for="flexRadioDefault2">
+                    先生
+                  </label>
+                </div>
+                <div className="form-check ms-2">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="flexRadioDefault"
+                    id="flexRadioDefault3"
+                  />
+                  <label className="form-check-label" for="flexRadioDefault3">
+                    其他
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="row mb-2">
+              <label htmlFor="cellphone" className="form-label col-form-label">
+                訂位人手機號碼
               </label>
               <div className="col-12">
                 <input
                   type="text"
-                  className="inputframe name"
-                  id="name"
-                  placeholder=" 請輸入姓名"
-                  autoFocus
+                  className="inputframe form-control"
+                  id="cellphone"
+                  placeholder=" 請輸入手機號碼"
                 />
               </div>
             </div>
-            <div className="ms-4 mt-3 fs18 d-flex align-self-end">
-              <div className="form-check">
+            <div className="row mb-2">
+              <label htmlFor="email" className="form-label col-form-label">
+                訂位人 email
+              </label>
+              <div className="col-12">
                 <input
-                  className="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault1"
-                  checked
+                  type="text"
+                  className="inputframe form-control"
+                  id="email"
+                  placeholder=" 請輸入 email"
                 />
-                <label className="form-check-label" for="flexRadioDefault1">
-                  小姐
-                </label>
-              </div>
-              <div className="form-check ms-2">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault2"
-                />
-                <label className="form-check-label" for="flexRadioDefault2">
-                  先生
-                </label>
-              </div>
-              <div className="form-check ms-2">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault3"
-                />
-                <label className="form-check-label" for="flexRadioDefault3">
-                  其他
-                </label>
               </div>
             </div>
-          </div>
-          <div className="row mb-2">
-            <label htmlFor="cellphone" className="form-label col-form-label">
-              訂位人手機號碼
-            </label>
-            <div className="col-12">
-              <input
-                type="text"
-                className="inputframe"
-                id="cellphone"
-                placeholder=" 請輸入手機號碼"
-              />
-            </div>
-          </div>
-          <div className="row mb-2">
-            <label htmlFor="email" className="form-label col-form-label">
-              訂位人 email
-            </label>
-            <div className="col-12">
-              <input
-                type="text"
-                className="inputframe"
-                id="email"
-                placeholder=" 請輸入 email"
-              />
-            </div>
-          </div>
-          <div className="row mb-2">
-            <label htmlFor="note" className="form-label col-form-label">
-              <p>備註 (例如: 少油少鹽)</p>
-            </label>
-            <div className="col-12">
-              <textarea name="note" id="note" cols="30" rows="10"></textarea>
+            <div className="row mb-2">
+              <label htmlFor="note" className="form-label col-form-label">
+                <p>備註 (例如: 少油少鹽)</p>
+              </label>
+              <div className="col-12">
+                <textarea
+                  className="form-control"
+                  name="note"
+                  id="note"
+                  cols="30"
+                  rows="10"
+                ></textarea>
+              </div>
             </div>
           </div>
         </div>
+        <br />
+        <br />
+        <div className="d-flex justify-content-center my-5">
+          <button className="btn btn-middle me-3">確認訂位</button>
+          <button className="btn btn-middle ms-3">回上一頁</button>
+        </div>
+        <br />
       </div>
       <Footer></Footer>
       <style jsx>
@@ -107,7 +175,21 @@ export default function Index() {
             width: 500px;
           }
           .container2 {
-            border: 1px solid;
+            width: 350px;
+            border: 1px solid grey;
+          }
+          .icon-map:before {
+            content: "\e905";
+            color: black;
+          }
+          .h5 {
+            line-height: 1.6rem;
+          }
+          .con1 {
+            border-bottom: 1px solid grey;
+          }
+          .container3 {
+            border: 1px solid grey;
             border-radius: 40px;
             width: 700px;
             height: 800px;
---
+++ team3next/pages/book/detail.js
@@ -0,0 +1,114 @@
+import Head from "next/head";
+import Navbar from "@/components/layout/default-layout/navbar-main/index";
+import Footer from "@/components/layout/default-layout/footer";
+import styles from "./restaurant.module.css";
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>食食嗑嗑-訂位詳細頁</title>
+      </Head>
+      <Navbar></Navbar>
+      <br />
+      <div className="container">
+        <div className="d-flex" style={{ marginLeft: "200px" }}>
+          <div
+            className="fs18 align-self-center"
+            style={{ marginRight: "80px" }}
+          >
+            <p className="h4">
+              Cin Cin Osteria
+              <br />
+              請請義大利餐廳
+            </p>
+            <br />
+            <div className="fs18">
+              <div className="d-flex">
+                <span className="pe-2">
+                  <span className="icon-map"></span>
+                </span>
+                <div>台南市中西區尊王路140號</div>
+              </div>
+              <div className="d-flex">
+                <span className="pe-2">
+                  <span className="icon-Call"></span>
+                </span>
+                <div>06 221 0699</div>
+              </div>
+              <div className="d-flex">
+                <span className="pe-2">
+                  <span className="icon-calender"></span>
+                </span>
+                <div>每週一、週二休息</div>
+              </div>
+            </div>
+          </div>
+          <div style={{ marginLeft: "80px" }}>
+            <img
+              style={{ width: "500px" }}
+              src="../../images/book/map-2.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <br />
+        <br />
+        <hr />
+        <br />
+        <br />
+        <div className="d-flex">
+          <div style={{ marginLeft: "200px", marginRight: "400px" }}>
+            <div className="h5 mb-4">用餐人數</div>
+            <div className="d-flex">
+              <div className="fs18 me-5 ps-3">3 位 大人</div>
+              <div className="fs18 ps-3">1 位 孩童</div>
+            </div>
+            <div className="h5 mt-5 mb-4">用餐時段</div>
+            <div className="fs18 ps-3">17:00</div>
+          </div>
+          <div>
+            <div className="h5 mb-4">用餐日期</div>
+            <div className="fs18 ps-3">11月7日 週二</div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <hr />
+        <br />
+        <br />
+        <div className="d-flex justify-content-center">
+          <div className="container3">
+            <div className="h5 mb-4">訂位人姓名</div>
+            <div className="fs18 ps-3">洪琪方 小姐</div>
+            <div className="h5 mb-4 mt-5">訂位人手機號碼</div>
+            <div className="fs18 ps-3">0963850549</div>
+            <div className="h5 mb-4 mt-5">訂位人 email</div>
+            <div className="fs18 ps-3">apple667733@gmail.com</div>
+            <div className="h5 mb-4 mt-5">其他備註</div>
+            <div className="fs18 ps-3">
+              請準備兒童餐具,
+              <br />
+              另外其中有一位對海鮮過敏, 再麻煩協助點菜。
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="container d-flex justify-content-center my-5">
+          <button className="btn btn-middle">取消訂位</button>
+        </div>
+      </div>
+      <Footer></Footer>
+      <style jsx>
+        {`
+          .container3 {
+            border: 1px solid #d9d9d9;
+            border-radius: 40px;
+            width: 700px;
+            padding: 100px;
+          }
+        `}
+      </style>
+    </>
+  );
+}
