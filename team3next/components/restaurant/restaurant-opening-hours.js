5ed1c142e9caed7d5ed328b49d0ec569447ed90f
Alex <wyltw812@gmail.com>
2023-09-29 21:07:29 +0800

    opening-hour-form-bug-solved

--- team3next/components/restaurant/restaurant-opening-hours.js
+++ team3next/components/restaurant/restaurant-opening-hours.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
+import dayjs from "dayjs";
 
 export default function OpeningHours() {
   const {
@@ -12,30 +13,70 @@ export default function OpeningHours() {
   } = useForm();
 
   const onSubmit = async (data) => {
-    console.log(data)
-    try{const response = await axios.post("http://localhost:3002/try-post",data);
-    console.log("Server Response:", response.data);}
-    catch(err){console.error("Error:",err)}
-    
-  }
+    console.log(data);
+    try {
+      const response = await axios.post("http://localhost:3002/try-post", data);
+      console.log("Server Response:", response.data);
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
 
-  const daysOfWeek = ["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
+  const daysOfWeek = [
+    "星期一",
+    "星期二",
+    "星期三",
+    "星期四",
+    "星期五",
+    "星期六",
+    "星期日",
+  ];
+  const hoursOption = [];
+  for (let i = 0; i < 24; i++) {
+    const hm = dayjs().hour(i).minute(0);
+    hoursOption.push(hm.format("HH:mm"));
+  }
+  //  算出24小時陣列
+  console.log(hoursOption);
   return (
     <>
       <div className="row">
         <div className="col-2"></div>
         <div className="col-8">
           <h2 style={{ color: "#985637" }}>營業時間管理</h2>
-          <form
-            onSubmit={handleSubmit(onSubmit)
-            }
-          >
+          <form onSubmit={handleSubmit(onSubmit)}>
             {daysOfWeek.map((v, i) => {
               return (
-                <label key={i} htmlFor={`day${i}`}>
-                  <input type="checkbox" id={`day${i}`} {...register("opening")} value={i} />
-                  {v}
-                </label>
+                <div key={i}>
+                  <label htmlFor={`day${i}`}>
+                    <input
+                      type="checkbox"
+                      id={`day${i}`}
+                      {...register(`weekday${i}`)}
+                      value={i == 6 ? i - 6 : i + 1}
+                    />
+                    {v}
+                    <select {...register(`startTime${i}`)}>
+                      {/*修正bug： 因為 startime 名稱相同，所以只會傳送最後一筆名為 starttime 的資料。 */}
+                      {hoursOption.map((v, i) => {
+                        return (
+                          <option key={i} value={v}>
+                            {v}
+                          </option>
+                        );
+                      })}
+                    </select>
+                    <select {...register(`endTime${i}`)}>
+                      {hoursOption.map((v, i) => {
+                        return (
+                          <option key={i} value={v}>
+                            {v}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </label>
+                </div>
               );
             })}
             <button type="submit">Send</button>
