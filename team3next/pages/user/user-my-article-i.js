0fb84d96bfd980964b9aab4585389e3d11d5676a
will8086 <will8086@gmail.com>
2023-09-30 10:42:50 +0800

    修正一些樣式

--- team3next/pages/user/user-my-article-i.js
+++ team3next/pages/user/user-my-article-i.js
@@ -2,11 +2,13 @@ import React from "react";
 import Link from "next/link";
 import Head from "next/head";
 import MyNavbar from "@/components/layout/default-layout/navbar-main";
+import WhoInfo from "@/components/user/who-info";
 
 export default function UserMyArticleI() {
   return (
     <>
     <MyNavbar></MyNavbar>
+    <WhoInfo></WhoInfo>
       <Head>
         <title>XXX的文章</title>
       </Head>
