827130d5520301cc259f670d4ad9965372f817c2
will8086 <will8086@gmail.com>
2023-09-30 13:39:42 +0800

    會員頁面樣式完成

---
+++ team3next/components/user/who-s-card.js
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Styles from "@/components/user/user-information.module.scss";
+import Link from "next/link";
+
+export default function WhoSCard() {
+  return (
+    <>
+      <div className={Styles.authorCard}>
+        <div className="d-flex align-items-center justify-content-between w-100">
+          <div className="d-flex ps-4">
+            <div className="pe-3">
+              <Link href="/user/user-my-article-i">
+                <img
+                  className="rounded-circle img-thumbnail"
+                  src="/images/logo.png"
+                  style={{ height: 60, width: 60, objectFit: "cover" }}></img>
+              </Link>
+            </div>
+            <div className="middle">
+              <Link className="fs16b text-dark" href="/user/user-my-article-i">
+                會員暱稱
+              </Link>
+            </div>
+          </div>
+          <div className="d-flex pe-4">
+            <button className="btn btn-little">追蹤中</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
