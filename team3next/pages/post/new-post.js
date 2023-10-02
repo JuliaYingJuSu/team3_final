7128142d46d3bb9425b5f0de61249853595960e1
JuliaYingJuS <yingjumr@gmail.com>
2023-10-02 15:24:17 +0800

    新增文章樣式套用user

--- team3next/pages/post/new-post.js
+++ team3next/pages/post/new-post.js
@@ -2,11 +2,16 @@ import AddPost from "@/components/post/add-post";
 import React from "react";
 import Navbar from "@/components/layout/default-layout/navbar-main";
 import Footer from "@/components/layout/default-layout/footer";
+import UserInfo from "@/components/user/user-info";
+import UserNavbar from "@/components/user/user-navbar";
 
 export default function NewPost() {
   return (
     <>
       <Navbar />
+      <UserInfo/>
+      <UserNavbar/>
+
       <AddPost />
       <Footer />
     </>
