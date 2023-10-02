697e8caab26878bcefe3105e1a9a1bfd6916cb29
JuliaYingJuS <yingjumr@gmail.com>
2023-10-02 15:16:26 +0800

    新增文章樣式變更

--- team3next/components/post/add-post.js
+++ team3next/components/post/add-post.js
@@ -3,126 +3,88 @@ import React from "react";
 export default function AddPost() {
   return (
     <>
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-lg modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                <div className="d-flex align-items-center p-1">
-                  <div className="me-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="51"
-                      height="50"
-                      viewBox="0 0 51 50"
-                      fill="none"
-                    >
-                      <circle cx="25.5" cy="25" r="25" fill="#DC8B76" />
-                    </svg>
-                  </div>
-                  <p className="middle me-2">
-                    <a className="fs16b pt-3 text-dark" href="#">
-                      會員暱稱
-                    </a>
-                  </p>
-                </div>
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">
-              <div
-                className="d-flex justify-content-center mb-3"
-                style={{ heigh: 300 }}
-              >
-                <img src="./images/post/image-gallery.png" alt="" />
-              </div>
-              <form action="">
-                <div class="input-group mb-3">
-                  <input
-                    type="file"
-                    class="form-control"
-                    id="inputGroupFile02"
-                    multiple
-                  />
-                  <label class="input-group-text" for="inputGroupFile02">
-                    上傳圖片
-                  </label>
-                </div>
-                <div class="input-group mb-3">
-                  <span
-                    class="input-group-text icon-edit"
-                    id="basic-addon1"
-                  ></span>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="新增標題"
-                    aria-label="Posttitle"
-                    aria-describedby="basic-addon1"
-                  />
-                </div>
-                <div class="input-group mb-3">
-                  <span class="input-group-text icon-map"></span>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="新增地點"
-                    aria-label="Postlocation"
-                    aria-describedby="button-addon2"
-                  />
-                  <button
-                    class="btn btn-outline-secondary"
-                    type="button"
-                    id="button-addon2"
-                    className="icon-arrow-s-right"
-                  ></button>
-                </div>
-                <div class="input-group mb-3">
-                  <span class="input-group-text icon-tag"></span>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="新增標籤"
-                    aria-label="Postlocation"
-                    aria-describedby="button-addon3"
-                  />
-                  <button
-                    class="btn btn-outline-secondary"
-                    type="button"
-                    id="button-addon3"
-                    className="icon-arrow-s-right"
-                  ></button>
-                </div>
-                <div class="input-group mb-3">
-                  <span class="input-group-text icon-edit"></span>
-                  <textarea
-                    class="form-control"
-                    aria-label="With textarea"
-                    placeholder="撰寫內文..."
-                    rows="5"
-                  ></textarea>
-                </div>
-                <div class="row align-items-end">
-                  <button type="submit" class="btn btn-primary">
-                    發表文章
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
+      <div className="container">
+        <div
+          className="d-flex justify-content-center mb-3"
+          style={{ heigh: 300 }}
+        >
+          <img src="/images/post/image-gallery.png" alt="" />
         </div>
+        <form action="">
+          <div className="input-group mb-3">
+            <input
+              type="file"
+              className="form-control"
+              id="inputGroupFile02"
+              multiple
+            />
+            <label className="input-group-text" for="inputGroupFile02">
+              上傳圖片
+            </label>
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text icon-edit" id="basic-addon1"></span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="新增標題"
+              aria-label="Posttitle"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text icon-map"></span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="新增地點"
+              aria-label="Postlocation"
+              aria-describedby="button-addon2"
+            />
+            {/* <button
+              className="icon-arrow-s-right btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
+            ></button> */}
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text icon-tag"></span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="新增標籤"
+              aria-label="Posttag"
+              aria-describedby="button-addon3"
+            />
+            <button
+              className="btn btn-outline-secondary icon-arrow-s-right"
+              type="button"
+              id="button-addon3"
+            ></button>
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text icon-edit"></span>
+            <textarea
+              className="form-control"
+              aria-label="With textarea"
+              placeholder="撰寫內文..."
+              rows="10"
+            ></textarea>
+          </div>
+          <div className="d-flex justify-content-center mb-3">
+            <button type="submit" className="btn btn-big">
+              發表文章
+            </button>
+          </div>
+        </form>
       </div>
+      <style jsx>
+        {`
+          .btn {
+            color: #ae4818;
+          }
+        `}
+      </style>
     </>
   );
 }
--- team3next/pages/post/new-post.js
+++ team3next/pages/post/new-post.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import AddPost from "@/components/post/add-post";
+import React from "react";
+import Navbar from "@/components/layout/default-layout/navbar-main";
+import Footer from "@/components/layout/default-layout/footer";
 
 export default function NewPost() {
   return (
-    <></>
-  )
+    <>
+      <Navbar />
+      <AddPost />
+      <Footer />
+    </>
+  );
 }
