1434699882e8537437e473678ac405818a03805f
will8086 <will8086@gmail.com>
2023-10-03 11:14:01 +0800

    修正

--- team3next/pages/_app.js
+++ team3next/pages/_app.js
@@ -4,15 +4,12 @@ import "@/styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "@/styles/style.css";
-import { AuthProviderJWT } from "@/hooks/use-auth-jwt";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
   return (
-    <AuthProviderJWT>
       <Component {...pageProps} />
-    </AuthProviderJWT>
   );
 }
