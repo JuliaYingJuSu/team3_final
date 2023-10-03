8f6db8ec629667aa189829a3508d7b70e6b012eb
will8086 <will8086@gmail.com>
2023-10-03 10:19:39 +0800

    增加hook

--- team3next/pages/_app.js
+++ team3next/pages/_app.js
@@ -1,13 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
-import "@/styles/globals.css"
-import "slick-carousel/slick/slick.css"; 
+import "@/styles/globals.css";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "@/styles/style.css"
+import "@/styles/style.css";
+import { AuthProviderJWT } from "@/hooks/use-auth-jwt";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <AuthProviderJWT>
+      <Component {...pageProps} />
+    </AuthProviderJWT>
+  );
 }
