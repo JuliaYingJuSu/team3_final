9af8a4bed37f3a1e7c2a60929a0b9fb1688d5159
JuliaYingJuS <yingjumr@gmail.com>
2023-10-09 21:29:19 +0800

    首頁台北市餐廳篩選後將user暱稱加入完成

--- team3node/routes/post.js
+++ team3node/routes/post.js
@@ -5,7 +5,7 @@ const postRouter = express.Router();
 
 //由此開始撰寫路由
 postRouter.get("/", async(req, res)=>{
-  const sql = `SELECT * from post join post_image on post.post_id = post_image.post_id join post_restaurant on post.post_restaurant_id = post_restaurant.post_restaurant_id join post_food_tag on post.post_id = post_food_tag.post_id join food_tag on food_tag.food_tag_id = post_food_tag.food_tag_id;`
+  const sql = `SELECT * from post join post_image on post.post_id = post_image.post_id join post_restaurant on post.post_restaurant_id = post_restaurant.post_restaurant_id join post_food_tag on post.post_id = post_food_tag.post_id join food_tag on food_tag.food_tag_id = post_food_tag.food_tag_id ORDER BY post.post_id DESC;`
 
   // const sql2 = `SELECT * FROM post join likes on post.post_id = likes.post_id;`
 
