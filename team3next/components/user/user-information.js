3c1288b2b532f899d40bcf4ef10fd174967cdb33
will8086 <will8086@gmail.com>
2023-10-15 10:55:47 +0800

    讀取卡片資料成功

--- team3next/components/user/user-information.js
+++ team3next/components/user/user-information.js
@@ -2,10 +2,22 @@ import React from "react";
 import UserButtonGroup from "./user-button-group";
 import Styles from "./user-information.module.scss";
 import AuthContext from "@/hooks/AuthContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function UserInformation() {
   const { auth } = useContext(AuthContext);
+
+  //隱藏or呈現密碼
+  const [show, setShow] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  console.log(errors);
+  // sweetalert設定
 
   return (
     <>
@@ -37,12 +49,18 @@ export default function UserInformation() {
                   <span style={{ color: "red" }} className="ps-1">
                     *
                   </span>
+                  <span className="ps-1" style={{ color: "red" }}>
+                    {errors.user_name?.message}
+                  </span>
                 </label>
                 <input
                   type="text"
-                  className="form-control input-f"
+                  className={`form-control input-f ${
+                    errors.user_name ? "is-invalid" : ""
+                  }`}
                   id="InputName"
                   value={auth.user_name}
+                  {...register("user_name", { required: "請輸入姓名" })}
                 />
               </div>
               <div className="mb-3">
@@ -51,27 +69,58 @@ export default function UserInformation() {
                   <span style={{ color: "red" }} className="ps-1">
                     *
                   </span>
+                  <span className="ps-1" style={{ color: "red" }}>
+                    {errors.nickname?.message}
+                  </span>
                 </label>
                 <input
                   type="text"
-                  className="form-control input-f"
+                  className={`form-control input-f ${
+                    errors.nickname ? "is-invalid" : ""
+                  }`}
                   id="InputNickName"
                   value={auth.nickname}
+                  {...register("nickname", { required: "請輸入暱稱" })}
                 />
               </div>
-              <div className="mb-3">
+              <div>
                 <label htmlFor="InputPassword" className="form-label fs18b">
                   密碼
                   <span style={{ color: "red" }} className="ps-1">
                     *
                   </span>
+                  <span className="ps-1" style={{ color: "red" }}>
+                    {errors.user_password?.message}
+                  </span>
                 </label>
                 <input
-                  type="password"
-                  className="form-control input-f"
+                  type={show ? "text" : "password"}
+                  className={`form-control input-f ${
+                    errors.user_password ? "is-invalid" : ""
+                  }`}
                   id="InputPassword"
                   value={auth.user_password}
+                  {...register("user_password", {
+                    required: "請輸入英文+數字至少8碼最多不超過12碼",
+                    minLength: {
+                      value: 8,
+                      message: "請輸入英文+數字至少8碼",
+                    },
+                    maxLength: {
+                      value: 12,
+                      message: "請不要超過12碼",
+                    },
+                  })}
                 />
+                <i
+                  type="button"
+                  className={`far ${
+                    show ? "fa-eye" : "fa-eye-slash"
+                  } no-see-eye`}
+                  style={{ color: "#787878" }}
+                  onClick={() => {
+                    setShow(!show);
+                  }}></i>
               </div>
               <div className="mb-3">
                 <label htmlFor="InputPhone" className="form-label fs18b">
@@ -79,12 +128,24 @@ export default function UserInformation() {
                   <span style={{ color: "red" }} className="ps-1">
                     *
                   </span>
+                  <span className="ps-1" style={{ color: "red" }}>
+                    {errors.user_phone?.message}
+                  </span>
                 </label>
                 <input
                   type="text"
-                  className="form-control input-f"
+                  className={`form-control input-f ${
+                    errors.user_phone ? "is-invalid" : ""
+                  }`}
                   id="InputPhone"
                   value={auth.user_phone}
+                  {...register("user_phone", {
+                    required: "請輸入09開頭共10碼的手機號碼",
+                    pattern: {
+                      value: /^(09)[0-9]{8}$/,
+                      message: "請輸入09開頭共10碼的手機號碼",
+                    },
+                  })}
                 />
               </div>
               <UserButtonGroup />
@@ -97,6 +158,18 @@ export default function UserInformation() {
           </div>
         </div>
       </div>
+      <style jsx>
+        {`
+          .no-see-eye {
+            position: relative;
+          }
+          .no-see-eye:before {
+            position: absolute;
+            left: 450px;
+            bottom: 32px;
+          }
+        `}
+      </style>
     </>
   );
 }
