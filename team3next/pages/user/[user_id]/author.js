1e2d7d68f68a70667afd9823bed499030b91f841
will8086 <will8086@gmail.com>
2023-10-15 22:01:21 +0800

    讀取追蹤作者

--- team3next/pages/user/[user_id]/author.js
+++ team3next/pages/user/[user_id]/author.js
@@ -1,4 +1,5 @@
-import React from "react";
+import AuthContext from "@/hooks/AuthContext";
+import { useEffect, useState, useContext } from "react";
 import MyNavbar from "@/components/layout/default-layout/navbar-main";
 import UserNavbar from "@/components/user/user-navbar";
 import Head from "next/head";
@@ -8,33 +9,56 @@ import Styles from "@/components/user/user-information.module.scss";
 import UserSCard from "@/components/user/user-s-card";
 
 export default function Author() {
+  const { auth } = useContext(AuthContext);
+  const [myauthor, setMyAuthor] = useState([]);
+
+  useEffect(() => {
+    fetch(process.env.API_SERVER + `/api/user/${auth.user_id}/myauthor`)
+      .then((r) => r.json())
+      .then((r) => {
+        setMyAuthor(r);
+        console.log(r);
+      })
+      .catch((ex) => {
+        console.log(ex);
+      });
+  }, [auth.user_id]);
+
   return (
     <>
       <MyNavbar></MyNavbar>
       <UserInfo></UserInfo>
       <UserNavbar />
-      <div className={"container mb-5" + " " + `${Styles.afs}`}>
-        <div className={Styles.authorText}>共有 XXX 位追蹤</div>
-        <div className="container mt-4">
-          <div className="row row-cols-auto g-4">
-            <div className="col">
-              <UserSCard></UserSCard>
-            </div>
-            <div className="col">
-              <UserSCard></UserSCard>
-            </div>
-            <div className="col">
-              <UserSCard></UserSCard>
-            </div>
-            <div className="col">
-              <UserSCard></UserSCard>
-            </div>
-            <div className="col">
-              <UserSCard></UserSCard>
+      {myauthor.length > 0 ? (
+        <div className={"container mb-5" + " " + `${Styles.afs}`}>
+          <div className={Styles.authorText}>共有 {myauthor.length} 位追蹤</div>
+          <div className="container mt-4">
+            <div className="row row-cols-auto g-4">
+              {myauthor.map((author, i) => {
+                return (
+                  <div className="col" key={i}>
+                    <UserSCard author={author}></UserSCard>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div
+          style={{
+            fontSize: 22,
+            color: "grey",
+            width: 800,
+            textAlign: "center",
+            marginTop: 45,
+            height: 400,
+            marginLeft: 20,
+          }}>
+          還沒有人追蹤喔~
+        </div>
+      )}
+
       <Footer></Footer>
       <Head>
         <title>追蹤作者</title>
